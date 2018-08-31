a4d474223332e615686cfbe50fda079f03e62d0a
netwok <37182648+netwok@users.noreply.github.com>
2018-08-31 21:27:33 +0100

    Update Papers Please.py

--- CodeWars/Papers Please.py
+++ CodeWars/Papers Please.py
@@ -1,21 +1,25 @@
 #Papers, Please - CodeWars
 
-countries = ['Arstotzka', 'Antegria', 'Impor', 'Kolechia',
-'Obristan', 'Republia', 'United Federation']
-foreign = ['Antegria', 'Impor', 'Kolechia',
-'Obristan', 'Republia', 'United Federation']
-vacc = {co:0 for co in countries}
-#polio==1, tetanus==2, both==3
-req = {co:[] for co in countries}
-
-class Game(object):
+class Inspector(object):
     def __init__(self):
         self.allowed = set([])
-    
-    def receiveBulletin(self, message):
-        self.outlaws = set([]) 
-        message = message.split('\n')
-        for line in message:
+        self.date = 19821122
+        self.countries = ['Arstotzka', 'Antegria', 'Impor', 'Kolechia', 'Obristan', 'Republia', 'United Federation']
+        self.polio = set([])
+        self.tetanus = set([])
+        self.passport = False
+        self.ID = set([])
+        self.permit = set([])
+        self.work_pass = set([])
+        self.grant_of_asylum = set([])
+        self.certificate_of_vaccination = set([])
+        self.diplomatic_authorization = set([])
+        
+    def receive_bulletin(self, message):
+        print(bulletin)
+        self.outlaw = ''
+        mess = message.split('\n')
+        for line in mess:
             if 'Allow' in line:
                 start = line.find('of ') + 3
                 self.allowed.update(line[start:].split(', '))
@@ -23,14 +27,44 @@ class Game(object):
                 start = line.find('of ') + 3
                 self.allowed = self.allowed.difference(line[start:].split(', '))
             if 'Wanted' in line:
-                start = line.find(': ') + 2
-                self.outlaws.update(line[start:].split(', '))
-            if 'permit' in line:
-                pass
+                self.outlaw = line[line.find(': ') + 2:]
             if 'polio' in line:
                 if 'no longer' in line:
-                   start = line.find('of ') + 3 
-                
+                   self.polio = set([])
+                else:
+                    start = line.find('of ') + 3
+                    c = line[start:line.find(' req')].split(', ')
+                    self.polio.update(c)
+            if 'tetanus' in line:
+                if 'no longer' in line:
+                   self.tetanus = set([])
+                else:
+                    start = line.find('of ') + 3
+                    c = line[start:line.find(' req')].split(', ')
+                    self.tetanus.update(c)
+            if "passport" in line:
+                self.passport = 'no' not in line
+            if "ID" in line:
+                start = line.find('of ') + 3
+                c = line[start:line.find(' req')].split(', ')
+                self.ID.update(c)
+            if 'permit' in line:
+                start = line.find('of ') + 3
+                c = line[start:line.find(' req')].split(', ')
+                self.permit.update(c)
+            if 'work pass' in line:
+                start = line.find('of ') + 3
+                c = line[start:line.find(' req')].split(', ')
+                self.work_pass.update(c)
+            if 'asylum' in line:
+                start = line.find('of ') + 3
+                c = line[start:line.find(' req')].split(', ')
+                self.grant_of_asylum.update(c) 
+            if 'diplomatic' in line:
+                start = line.find('of ') + 3
+                c = line[start:line.find(' req')].split(', ')
+                self.diplomatic_authorization.update(c)
+
     def inspect(self, entrant):
         #first check the names for criminals
         #if outlaws in entrant.names: return 'Detained etc.'
@@ -45,18 +79,12 @@ class Entrant(object):
     def __init__(self, description):
         self.description = description
         self.docs = description.keys()
-        self.passport = description.get('passport', False)
-        self.ID_card = description.get('ID_card', False)
-        self.access_permit = description.get('access_permit', False)
-        self.work_pass = description.get('work_pass', False)
-        self.grant_of_asylum  = description.get('grant_of_asylum', False)
-        self.certificate_of_vaccination  = description.get('certificate_of_vaccination', False)
-        self.diplomatic_authorization  = description.get('diplomatic_authorization', False)
-        
-        self.names = set(self.get_detail(self.description[doc], 'NAME: ') for doc in self.docs)
-        self.IDs = set(self.get_detail(self.description[doc], 'ID#: ') for doc in self.docs)
-        self.DOBs = set(self.get_detail(self.description[doc], 'DOB: ') for doc in self.docs)
-        self.nations = set(self.get_detail(self.description[doc], 'NATION: ') for doc in self.docs)
+        self.name = set(self.get_detail(self.description[doc], 'NAME: ') for doc in self.docs)
+        self.ID = set(self.get_detail(self.description[doc], 'ID#: ') for doc in self.docs)
+        self.DOB = set(self.get_detail(self.description[doc], 'DOB: ') for doc in self.docs)
+        self.nation = set(self.get_detail(self.description[doc], 'NATION: ') for doc in self.docs)
+        self.EXP = set(self.get_detail(self.description[doc], 'EXP: ') for doc in self.docs)
+        self.sex = set(self.get_detail(self.description[doc], 'SEX: ') for doc in self.docs)
         
     
     def get_detail(self, doc, code):
@@ -81,4 +109,12 @@ roman = {
 	"passport": 'ID#: WK9XA-LKM0Q\nNATION: United Federation\nNAME: Dolanski, Roman\nDOB: 1933.01.01\nSEX: M\nISS: Shingleton\nEXP: 1983.05.12',
 	"grant_of_asylum": 'NAME: Dolanski, Roman\nNATION: United Federation\nID#: Y3MNC-TPWQ2\nDOB: 1933.01.01\nHEIGHT: 176cm\nWEIGHT: 71kg\nEXP: 1983.09.20'
 }
-entrant = Entrant(roman)
+
+inspector = Inspector()
+bulletin2 = """Entrants require passport
+Allow citizens of Arstotzka, Obristan
+Citizens of Arstotzka, Obristan require polio vaccination"""
+
+inspector.receive_bulletin(bulletin2)
+
+

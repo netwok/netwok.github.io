3668c5b368f6f0f551c612e92365664f340ba64e
netwok <37182648+netwok@users.noreply.github.com>
2018-09-13 21:08:42 +0100

    Update things_messy.py

--- CodeWars/things_messy.py
+++ CodeWars/things_messy.py
@@ -1,43 +1,22 @@
- class Thing(object):
-    def __init__(self, aka, plural = False):
-        self.name = aka[:-1 if plural else None]
+class Thing(object):
+    def __init__(self, name, plural = False):
+        self.name = name[:-1 if plural else None]
         if 'desc' not in dir(self):
-            self.desc = {'is_'+aka.lower()[:-1 if plural else None]:True}
+            self.desc = {'is_'+name.lower()[:-1 if plural else None]:True}
         self.is_a = IsA(self, True)
         self.is_not_a = IsA(self, False)
         self.is_the = IsThe(self)
         self.being_the = IsThe(self)
         self.has = lambda num: HasSome(self, num)
         self.having = self.has
+        self.can = Can(self)
     
     def __getattr__(self, attr):
-        if attr == 'can':
-            return self.Can(self)
+        name = self.name
         if attr in self.desc:
             return self.desc[attr]
         else:
             print('undefined')
-            
-                
-    class Can(object):
-        def __init__(self, thing):
-            self.thing = thing
-        
-        def can_do(self, method, past=''):
-            def decorator(func):
-                def decorated(*args, **kwargs):
-                    if past:
-                        if past in self.thing.desc:
-                            self.thing.desc[past].append(method(*args))
-                        else: self.thing.desc[past] = [method(*args)]
-                    return method(*args, **kwargs)
-                return decorated
-        
-            self.thing.desc = {**self.thing.desc, self.verb: decorator(method)} #thing can now do the verb, and remember its args
-            
-        def __getattr__(self, verb):  
-            self.verb = verb
-            return self.can_do
             
 class IsA(Thing):
     def __init__(self, parent, boo):
@@ -61,6 +40,7 @@ class ThingOf(object):
     def __getattr__(self, relative):
         if relative in self.thing_of_desc:
             return self.thing_of_desc[relative]
+        print('calling ThingOf\'s getattr on',relative)
         self.thing.desc = {**self.thing.desc, self.name:relative}
         
 class HasSome(object):
@@ -112,3 +92,28 @@ class EachHaving(Thing):
             thing.has(self.num).__getattr__(method)
             tuple_thing = type('tuple_Thing', (tuple,), {'each':Each(tuple(thing.__getattr__(method) for thing in self.things))})
         return tuple_thing().each
+        
+class Can(Thing):
+    def __init__(self, thing):
+        self.thing = thing
+    
+    def can_do(self, method, past=''):
+        #.can.verb(method, past='')
+        #decorate method so it repeats when called on empty
+        def decorator(func):
+            def decorated(*args, **kwargs):
+                func.__globals__['name'] = self.thing.name
+                if past:
+                    if past in self.thing.desc:
+                        self.thing.desc[past].append(func(*args, **kwargs))
+                    else:
+                        self.thing.desc = {**self.thing.desc, past: [func(*args, **kwargs)]}
+                return func(*args, **kwargs)
+            return decorated 
+    
+        self.thing.desc = {**self.thing.desc, self.verb: decorator(method)}
+        
+    def __getattr__(self, verb):  
+        self.verb = verb
+        return self.can_do
+    
